fe232e50c8f6c3d19e673c147111b3f25a124a92
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-12-11 19:47:26 +0200

    feat: set up express server and integrate genre router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
+import express from "express";
 import AppDataSource from "./database/data-source.js";
+import genreRouter from "./genre/genre.router.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/genres", genreRouter);
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("Database is running");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
   })
   .catch((error) =>
     console.log("Error during Data Source initialization:", error)
