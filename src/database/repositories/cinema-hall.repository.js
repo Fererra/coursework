ecbbee77f07077b434ad442e5713a46522b47e18
Anasstassik <sherloch3k@gmail.com>
2025-12-13 09:45:24 +0200

    feat: add cinema hall management functionality with CRUD operations

---
+++ src/database/repositories/cinema-hall.repository.js
@@ -0,0 +1,161 @@
+import AppDataSource from "../data-source.js";
+import { CinemaHallErrorMessages } from "../../cinema-hall/cinema-hall.errors.js";
+
+class CinemaHallRepository {
+  #repo;
+  #dataSource;
+
+  constructor(dataSource) {
+    this.#repo = dataSource.getRepository("CinemaHall");
+    this.#dataSource = dataSource;
+  }
+
+  getAllCinemaHalls() {
+    return this.#repo.find({
+      select: ["hallId", "hallNumber", "capacity"],
+      where: { deletedAt: null },
+    });
+  }
+
+  getCinemaHallDetails(hallId) {
+    return this.#repo
+      .createQueryBuilder("cinemaHall")
+      .leftJoinAndSelect("cinemaHall.seats", "seats", "seats.deletedAt IS NULL")
+      .select([
+        "cinemaHall.hallId",
+        "cinemaHall.hallNumber",
+        "cinemaHall.capacity",
+        "seats.seatId",
+        "seats.rowNumber",
+        "seats.seatNumber",
+        "seats.seatType",
+        "seats.basePrice",
+      ])
+      .where("cinemaHall.hallId = :cinemaHallId", { cinemaHallId: hallId })
+      .andWhere("cinemaHall.deletedAt IS NULL")
+      .getOne();
+  }
+
+  async createCinemaHall(data) {
+    return this.#dataSource.transaction(async (manager) => {
+      try {
+        const { seats, ...cinemaHallData } = data;
+
+        const cinemaHall = manager.create("CinemaHall", cinemaHallData);
+        await manager.save("CinemaHall", cinemaHall);
+
+        if (seats && seats.length > 0) {
+          const seatEntities = seats.map((seat) =>
+            manager.create("Seat", {
+              ...seat,
+              hallId: cinemaHall.hallId,
+            })
+          );
+
+          cinemaHall.seats = seatEntities;
+
+          await manager.save("Seat", seatEntities);
+        }
+
+        return cinemaHall;
+      } catch (error) {
+        if (error.code === "23505") {
+          throw new Error(CinemaHallErrorMessages.CINEMA_HALL_ALREADY_EXISTS);
+        }
+        throw error;
+      }
+    });
+  }
+
+  async updateCinemaHall(hallId, updateData) {
+    return this.#dataSource.transaction(async (manager) => {
+      const { seats, ...updateHallData } = updateData;
+
+      const cinemaHall = await manager.findOne("CinemaHall", {
+        where: { hallId, deletedAt: null },
+        relations: ["seats"],
+      });
+
+      if (!cinemaHall)
+        throw new Error(CinemaHallErrorMessages.CINEMA_HALL_NOT_FOUND);
+
+      Object.assign(cinemaHall, updateHallData);
+
+      if (seats) {
+        for (const seatDto of seats) {
+          console.log(seatDto.seatId);
+          if (seatDto.seatId) {
+            const existingSeat = cinemaHall.seats.find(
+              (s) => s.seatId === seatDto.seatId
+            );
+
+            if (!existingSeat) throw new Error("Seat not found");
+
+            console.log(seatDto);
+
+            if (seatDto.deleted) {
+              await manager.softDelete("Seat", { seatId: seatDto.seatId });
+            } else {
+              Object.assign(existingSeat, seatDto);
+            }
+          } else {
+            const newSeat = manager.create("Seat", {
+              ...seatDto,
+              hallId: cinemaHall.hallId,
+            });
+
+            cinemaHall.seats.push(newSeat);
+          }
+        }
+      }
+
+      try {
+        await manager.save("CinemaHall", cinemaHall);
+
+        return manager.findOne("CinemaHall", {
+          where: { hallId },
+          relations: ["seats"],
+          withDeleted: false,
+        });
+      } catch (error) {
+        if (error.code === "23505") {
+          throw new Error(CinemaHallErrorMessages.CINEMA_HALL_ALREADY_EXISTS);
+        }
+        throw error;
+      }
+    });
+  }
+
+  async deleteCinemaHall(hallId) {
+    return this.#dataSource.transaction(async (manager) => {
+      const hall = await manager
+        .getRepository("CinemaHall")
+        .createQueryBuilder("cinemahall")
+        .leftJoinAndSelect("cinemahall.seats", "seat", "seat.deletedAt IS NULL")
+        .leftJoinAndSelect(
+          "cinemahall.showtimes",
+          "showtime",
+          "showtime.deletedAt IS NULL"
+        )
+        .where("cinemahall.hallId = :cinemaHallId", { hallId })
+        .andWhere("cinemahall.deletedAt IS NULL")
+        .getOne();
+
+      if (!hall) throw new Error(CinemaHallErrorMessages.CINEMA_HALL_NOT_FOUND);
+
+      if (hall.showtimes.length > 0) {
+        throw new Error(CinemaHallErrorMessages.CINEMA_HALL_DELETE_ERROR);
+      }
+
+      for (const seat of hall.seats) {
+        await manager.softDelete("Seat", { seatId: seat.seatId });
+      }
+
+      await manager.softDelete("CinemaHall", { hallId });
+
+      return { message: "Cinema hall deleted successfully" };
+    });
+  }
+}
+
+export const cinemaHallRepository = new CinemaHallRepository(AppDataSource);
