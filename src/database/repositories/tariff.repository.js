2c3ab6de8f23fe5036bfa832e2d705c35596b40d
Sonya010 <kozachsofia@gmail.com>
2025-12-15 12:42:29 +0200

    refactor: enhance error handling in TariffRepository and add booking-related error messages

--- src/database/repositories/tariff.repository.js
+++ src/database/repositories/tariff.repository.js
@@ -1,5 +1,7 @@
 import AppDataSource from "../data-source.js";
 import { TariffErrorMessages } from "../../modules/tariff/tariff.errors.js";
+import { handleDatabaseError } from "../../common/utils/db-errors.js";
+import { BookingSeatStatus } from "../../modules/booking/booking-seat-status.js";
 
 class TariffRepository {
   #repo;
@@ -18,52 +20,51 @@ class TariffRepository {
   }
 
   async createTariff(data) {
-    return this.#dataSource.transaction(async (manager) => {
-      try {
-        const tariff = manager.create("Tariff", data);
-        await manager.save("Tariff", tariff);
-        return tariff;
-      } catch (error) {
-        if (error.code === "23505") {
-          throw new Error(TariffErrorMessages.TARIFF_ALREADY_EXISTS);
-        }
-        throw error;
-      }
-    });
+    try {
+      return await this.#repo.save(data);
+    } catch (error) {
+      handleDatabaseError(error, TariffErrorMessages.TARIFF_ALREADY_EXISTS);
+    }
   }
 
   async updateTariff(tariffId, updateData) {
-    return this.#dataSource.transaction(async (manager) => {
-      const tariff = await manager.findOne("Tariff", {
-        select: ["tariffId", "name"],
-        where: { tariffId, deletedAt: null },
-      });
+    try {
+      return await this.#dataSource.transaction(async (manager) => {
+        const tariff = await manager.findOne("Tariff", {
+          select: ["tariffId", "name"],
+          where: { tariffId, deletedAt: null },
+          lock: { mode: "pessimistic_write" },
+        });
 
-      if (!tariff) {
-        throw new Error(TariffErrorMessages.TARIFF_NOT_FOUND);
-      }
-
-      Object.assign(tariff, updateData);
-
-      try {
-        await manager.save("Tariff", tariff);
-        return tariff;
-      } catch (error) {
-        if (error.code === "23505") {
-          throw new Error(TariffErrorMessages.TARIFF_ALREADY_EXISTS);
+        if (!tariff) {
+          throw new Error(TariffErrorMessages.TARIFF_NOT_FOUND);
         }
-        throw error;
-      }
-    });
+
+        Object.assign(tariff, updateData);
+
+        return await manager.save("Tariff", tariff);
+      });
+    } catch (error) {
+      handleDatabaseError(error, TariffErrorMessages.TARIFF_ALREADY_EXISTS);
+    }
   }
 
   async deleteTariff(tariffId) {
     return this.#dataSource.transaction(async (manager) => {
       const tariff = await manager.findOne("Tariff", {
         where: { tariffId, deletedAt: null },
+        lock: { mode: "pessimistic_write" },
       });
 
       if (!tariff) throw new Error(TariffErrorMessages.TARIFF_NOT_FOUND);
+
+      const bookingCount = await manager.count("BookingSeat", {
+        where: { tariffId, status: BookingSeatStatus.ACTIVE },
+      });
+
+      if (bookingCount > 0) {
+        throw new Error(TariffErrorMessages.TARIFF_HAS_BOOKINGS);
+      }
 
       await manager.softDelete("Tariff", { tariffId });
 
