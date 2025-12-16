977399e5a16ccd2458851ea85bf9f38ae70e69b7
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-12-16 21:24:45 +0200

    refactor: remove async from repository methods and enhance error handling in service layer

--- src/database/repositories/movie.repository.js
+++ src/database/repositories/movie.repository.js
@@ -1,7 +1,6 @@
 import AppDataSource from "../data-source.js";
 import { MovieErrorMessages } from "../../modules/movie/movie.errors.js";
 import { In } from "typeorm";
-import { handleDatabaseError } from "../../common/utils/db-errors.js";
 import { GenreErrorMessages } from "../../modules/genre/genre.errors.js";
 
 class MovieRepository {
@@ -13,7 +12,7 @@ class MovieRepository {
     this.#dataSource = dataSource;
   }
 
-  async getAllMovies(page, pageSize) {
+  getAllMovies(page, pageSize) {
     return this.#repo
       .createQueryBuilder("movie")
       .select([
@@ -49,76 +48,68 @@ class MovieRepository {
       .getOne();
   }
 
-  async createMovie(data) {
-    try {
-      return await this.#dataSource.transaction(async (manager) => {
-        const { genreIds, ...movieData } = data;
+  createMovie(data) {
+    return this.#dataSource.transaction(async (manager) => {
+      const { genreIds, ...movieData } = data;
 
-        const movie = manager.create("Movie", movieData);
+      const movie = manager.create("Movie", movieData);
 
-        if (genreIds && genreIds.length > 0) {
-          const genres = await manager.find("Genre", {
-            where: { genreId: In(genreIds) },
-          });
-
-          if (genres.length !== genreIds.length) {
-            throw new Error(GenreErrorMessages.SOME_GENRES_NOT_FOUND);
-          }
-
-          movie.genres = genres;
-        }
-
-        return await manager.save("Movie", movie);
-      });
-    } catch (error) {
-      handleDatabaseError(error, MovieErrorMessages.MOVIE_ALREADY_EXISTS);
-    }
-  }
-
-  async updateMovie(movieId, updateData) {
-    try {
-      return this.#dataSource.transaction(async (manager) => {
-        const { genreIds, ...movieUpdateData } = updateData;
-
-        const movie = await manager.findOne("Movie", {
-          select: [
-            "movieId",
-            "title",
-            "ageLimit",
-            "durationMin",
-            "releaseYear",
-            "description",
-          ],
-          where: { movieId, deletedAt: null },
-          lock: { mode: "pessimistic_write" },
+      if (genreIds && genreIds.length > 0) {
+        const genres = await manager.find("Genre", {
+          where: { genreId: In(genreIds) },
         });
 
-        if (!movie) {
-          throw new Error(MovieErrorMessages.MOVIE_NOT_FOUND);
+        if (genres.length !== genreIds.length) {
+          throw new Error(GenreErrorMessages.SOME_GENRES_NOT_FOUND);
         }
 
-        Object.assign(movie, movieUpdateData);
+        movie.genres = genres;
+      }
 
-        if (genreIds && genreIds.length > 0) {
-          const genres = await manager.find("Genre", {
-            where: { genreId: In(genreIds), deletedAt: null },
-          });
-
-          if (genres.length !== genreIds.length) {
-            throw new Error(GenreErrorMessages.SOME_GENRES_NOT_FOUND);
-          }
-
-          movie.genres = genres;
-        }
-
-        return await manager.save("Movie", movie);
-      });
-    } catch (error) {
-      handleDatabaseError(error, MovieErrorMessages.MOVIE_ALREADY_EXISTS);
-    }
+      return manager.save("Movie", movie);
+    });
   }
 
-  async deleteMovie(movieId) {
+  updateMovie(movieId, updateData) {
+    return this.#dataSource.transaction(async (manager) => {
+      const { genreIds, ...movieUpdateData } = updateData;
+
+      const movie = await manager.findOne("Movie", {
+        select: [
+          "movieId",
+          "title",
+          "ageLimit",
+          "durationMin",
+          "releaseYear",
+          "description",
+        ],
+        where: { movieId, deletedAt: null },
+        lock: { mode: "pessimistic_write" },
+      });
+
+      if (!movie) {
+        throw new Error(MovieErrorMessages.MOVIE_NOT_FOUND);
+      }
+
+      Object.assign(movie, movieUpdateData);
+
+      if (genreIds && genreIds.length > 0) {
+        const genres = await manager.find("Genre", {
+          where: { genreId: In(genreIds), deletedAt: null },
+        });
+
+        if (genres.length !== genreIds.length) {
+          throw new Error(GenreErrorMessages.SOME_GENRES_NOT_FOUND);
+        }
+
+        movie.genres = genres;
+      }
+
+      return manager.save("Movie", movie);
+    });
+  }
+
+  deleteMovie(movieId) {
     return this.#dataSource.transaction(async (manager) => {
       const movie = await manager.findOne("Movie", {
         where: { movieId, deletedAt: null },
